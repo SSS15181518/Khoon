ab48aaec64fd56329c7b5816b2c23cf8c4b478f3
Subha Sourav Singh <subhasourav.singh77@gmail.com>
2018-10-28 07:35:58 +0530

    Bug fixes

--- app/src/main/java/com/weather/sss/khoon/aadharverifydonate.java
+++ app/src/main/java/com/weather/sss/khoon/aadharverifydonate.java
@@ -5,19 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class aadharverifydonate extends AppCompatActivity {
-    EditText aadhar;
+    EditText aadhar,otp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aadharverifydonate);
         aadhar = findViewById(R.id.editText);
+        otp = findViewById(R.id.editText2);
     }
     public void donateBlood(View view)
     {
+        if(otp.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Enter OTP and Verify",Toast.LENGTH_LONG).show();
+        }
+        else if(otp.getText().toString().equals("1234")){
         Intent intent = new Intent(getApplication(),signup.class);
         intent.putExtra("aadhar", aadhar.getText().toString());
         startActivity(intent);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Incorrect OTP",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
